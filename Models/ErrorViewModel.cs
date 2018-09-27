a1ebd7638c839610875a76fbd8554c7564b492f6
SURASAKK <surasaksermsakul@gmail.com>
2018-09-27 17:03:58 +0700

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HelloWolrd_CLI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
